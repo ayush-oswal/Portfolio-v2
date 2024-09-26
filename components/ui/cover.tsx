c11eb9804bfd18977e993d0546cb83403ad22660
ayush-oswal <ayushoswal2003@gmail.com>
2024-09-26 13:09:22 +0530

    Changed svgs to https

--- components/ui/cover.tsx
+++ components/ui/cover.tsx
@@ -168,7 +168,7 @@ export const Beam = ({
       height="1"
       viewBox={`0 0 ${width ?? "600"} 1`}
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns="https://www.w3.org/2000/svg"
       className={cn("absolute inset-x-0 w-full", className)}
       {...svgProps}
     >
