d300ddf84cff60aea4e11a0a213ffe4c0a77e38e
ayush-oswal <ayushoswal2003@gmail.com>
2024-10-02 12:58:14 +0530

    Added microsoft web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +39,15 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "oc7yddme4j");
+          `}
+        </Script>
         </ThemeProvider>
       </body>
     </html>
